8c620236b87bd08036b188181df44e34fd640133
ZodiaComs <125735027+ZodiaComs@users.noreply.github.com>
2023-02-21 12:28:59 +0800

    Add files via upload

---
+++ FirstJava.java
@@ -0,0 +1,7 @@
+public class FirstJava {
+public static void main (String [] man) {
+System.out.println ("hello Java.");
+System.out.println ("hello World.");
+System.out.println ("goodbye world.");
+}
+}
