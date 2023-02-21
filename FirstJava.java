51f8b6144fc8e2a2616ce4827d2f9503ad0a35df
ZodiaComs <125735027+ZodiaComs@users.noreply.github.com>
2023-02-21 12:41:57 +0800

    Delete FirstJava.java

--- FirstJava.java
+++
@@ -1,7 +0,0 @@
-public class FirstJava {
-public static void main (String [] man) {
-System.out.println ("hello Java.");
-System.out.println ("hello World.");
-System.out.println ("goodbye world.");
-}
-}
